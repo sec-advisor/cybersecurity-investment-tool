04856a29088127ec65738202dddc1e5ab9e8e992
Christian Omlin <christian.omlin@siemens.com>
2021-12-27 19:38:05 +0100

    refacotor: MENTOR API

--- apps/api/src/app/recommendation/services/recommendation.service.ts
+++ apps/api/src/app/recommendation/services/recommendation.service.ts
@@ -10,7 +10,8 @@ export class RecommendationService {
   recommend(body: RecommendationProfile): Observable<any> {
     // TODO CH: solve
     // const url = 'http://ml_decision_server:5000';
-    const url = 'http://192.168.200.130:5000'
+    // const url = 'http://192.168.200.130:5000'
+    const url = 'http://localhost:5000';
     return this.httpService.post(`${url}/v1/recommend`, body).pipe(map(response => response.data.recommendedServices));
   }
 
