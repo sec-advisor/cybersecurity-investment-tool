9e468228eda84216838ba32d6796c6d58602db27
Oliver Kamer <oliver@kamer.ch>
2022-08-30 17:38:22 +0200

    Black for mentor

--- mentor/engine/schemas/upload_service_schema.py
+++ mentor/engine/schemas/upload_service_schema.py
@@ -1,16 +1,25 @@
 from schema import Schema, And, Use
-from engine.helpers.const.service_characteristics import TYPE, REGIONS, DEPLOYMENT_TIME, LEASING_PERIOD
+from engine.helpers.const.service_characteristics import (
+    TYPE,
+    REGIONS,
+    DEPLOYMENT_TIME,
+    LEASING_PERIOD,
+)
 
-upload_service_schema = Schema({'productName': Use(str),
-                                'Service':  Use(str),
-                                'logo':  Use(str),
-                                'fileName':  Use(str),
-                                'description':  Use(str),
-                                'region':  [And(str, Use(str.upper), lambda s: s in REGIONS)],
-                                'serviceType': [And(str, Use(str.upper), lambda s: s in TYPE)],
-                                'attackType': [And(Use(str.upper))],
-                                'deploymentTime': And(str, Use(str.upper), lambda s: s in DEPLOYMENT_TIME),
-                                'leasingPeriod': And(str, Use(str.upper), lambda s: s in LEASING_PERIOD),
-                                'price': And(Use(int), lambda n: n > 0),
-                                'txHash': Use(str),
-                                'serviceHash': Use(str)})
+upload_service_schema = Schema(
+    {
+        "productName": Use(str),
+        "Service": Use(str),
+        "logo": Use(str),
+        "fileName": Use(str),
+        "description": Use(str),
+        "region": [And(str, Use(str.upper), lambda s: s in REGIONS)],
+        "serviceType": [And(str, Use(str.upper), lambda s: s in TYPE)],
+        "attackType": [And(Use(str.upper))],
+        "deploymentTime": And(str, Use(str.upper), lambda s: s in DEPLOYMENT_TIME),
+        "leasingPeriod": And(str, Use(str.upper), lambda s: s in LEASING_PERIOD),
+        "price": And(Use(int), lambda n: n > 0),
+        "txHash": Use(str),
+        "serviceHash": Use(str),
+    }
+)
