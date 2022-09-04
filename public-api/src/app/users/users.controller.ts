7e98d4ff30fa0b13f1b28bbc4d08fe93d069b141
Christian Omlin <christian.omlin@siemens.com>
2022-09-04 20:25:49 +0200

    refactor(public-api): define type for user request header

--- public-api/src/app/users/users.controller.ts
+++ public-api/src/app/users/users.controller.ts
@@ -11,6 +11,7 @@ import { from, map, Observable, of, switchMap } from 'rxjs';
 
 import { User } from '../../../libs/api-interfaces';
 import { LocalAuthGuard } from '../auth/local.auth.guard';
+import { UserRequest } from '../models/request.type';
 import { UsersService } from './services/users.service';
 
 @Controller('users')
@@ -36,17 +37,17 @@ export class UsersController {
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  login(@Request() req): Observable<User> {
-    return req.user;
+  login(@Request() request: UserRequest): Observable<User> {
+    return of(request.user);
   }
 
   @Post('/logout')
-  logout(@Request() req): void {
-    req.session.destroy();
+  logout(@Request() req: UserRequest): void {
+    req.session.destroy(() => undefined);
   }
 
   @Get('/is-active')
-  isActive(@Request() req): Observable<boolean> {
-    return of(!!req.user);
+  isActive(@Request() request: UserRequest): Observable<boolean> {
+    return of(!!request.user);
   }
 }
