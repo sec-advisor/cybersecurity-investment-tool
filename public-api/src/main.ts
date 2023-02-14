24f8eb274b06c79d66f2f61789d4b79dfe75dad6
Oliver Kamer <oliver@kamer.ch>
2023-02-14 13:42:57 +0100

    Put public api behind docker compose as well

--- public-api/src/main.ts
+++ public-api/src/main.ts
@@ -11,6 +11,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   app.enableCors({
     origin: [
